8a1ce3c082e8280e5c079f21c6b2b36d4f24eda5
victorzarzar <victorzarzar58@gmail.com>
2024-08-25 12:15:02 -0300

    Update in intropage and homepage and add translations assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:scan_pdf/pages/IntroPage/intro_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('pt', 'BR'),
+      ],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en-US'),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -15,10 +28,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Scanner PDF",
-      home: IntroPage(),
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: const IntroPage(),
     );
   }
 }
--- lib/pages/IntroPage/intro_page.dart
+++ lib/pages/IntroPage/intro_page.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/material.dart';
+import "package:easy_localization/easy_localization.dart";
+import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
+import 'package:getwidget/getwidget.dart';
+import "package:scan_pdf/pages/HomePage/home_page.dart";
 
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
@@ -10,6 +14,54 @@ class IntroPage extends StatefulWidget {
 class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double myHeight = MediaQuery.of(context).size.height;
+    double myWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        height: myHeight,
+        width: myWidth,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset('assets/imgs/icon.png'),
+            Text(
+              "intro_title".tr(),
+              style: GoogleFonts.jetBrainsMono(
+                textStyle: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40),
+              child: SizedBox(
+                height: 35,
+                width: 180,
+                child: GFButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomePage(),
+                      ),
+                    );
+                  },
+                  text: "intro_button".tr(),
+                  textStyle: TextStyle(
+                    color: Colors.red,
+                    fontFamily: GoogleFonts.jetBrainsMono().fontFamily,
+                    fontSize: 11,
+                  ),
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
